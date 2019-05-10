804b475ef27fcba2d949a06312d1d8dbc949e94d
theliang <theliang@163.com>
2019-05-10 17:07:42 +0800

    SpringBoot2.1与SpringSecurity5版本之间的兼容问题 完成SpringSecurity的基本使用： 1. 基本登录 2. 拦截权限，显示用户信息

---
+++ security-in-action/src/main/java/com/theliang/blog/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.theliang.blog.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * 安全配置类
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * 自定义配置
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/css/**", "/js/**", "/fonts/**", "/index").permitAll() // 都可以访问
+                .antMatchers("/h2-console/**").permitAll() // 都可以访问
+                .antMatchers("/users/**").hasRole("ADMIN") // 需要相应的角色才能访问
+                .and()
+                .formLogin()   //基于 Form 表单登录验证
+                .loginPage("/login").failureUrl("/login-error"); // 自定义登录界面，自动拦截login的post请求
+    }
+
+    /**
+     * 认证信息管理
+     */
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication() //认证信息存储在内存中
+                .passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("admin").password(new BCryptPasswordEncoder().encode("123123")).roles("ADMIN");
+
+    }
+}
